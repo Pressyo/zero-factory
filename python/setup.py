cfffdc9ec17eb793452e5c29757dce425b156fbe
chewxy <chewxy@gmail.com>
2012-10-21 02:38:56 +1100

    version 0.1.6 now

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='ZeroFactory',
-    version='0.1.5',
+    version='0.1.6',
     author='Xuanyi Chew',
     author_email='chewxy@gmail.com',
     packages=['zeroFactory'],
